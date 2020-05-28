ab9107094f2e403cebc70e5ed5677a6c29a5228a
Sonia Dumitru <soni.dumitru@gmail.com>
2020-05-28 15:17:00 -0400

    horizontal and showHorizontalScrollIndicator property

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -3,20 +3,41 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        {name: 'sonia'},
-        {name: 'diana'},
-        {name: 'lili'}
+        { name: 'sonia' },
+        { name: 'diana' },
+        { name: 'lili' },
+        { name: 'sonia' },
+        { name: 'diana' },
+        { name: 'sonia' },
+        { name: 'diana' },
+        { name: 'lili' },
+        { name: 'sonia' },
+        { name: 'diana' },
+        { name: 'sonia' },
+        { name: 'diana' },
+        { name: 'sonia' },
+        { name: 'diana' },
+        { name: 'lili' },
+        { name: 'sonia' },
+        { name: 'diana' }
     ]
     return (
         <FlatList 
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(friend) => friend.name}
             data={friends} 
             renderItem={({item}) => {
-            return <Text key={item.name}>{item.name}</Text>
+            return <Text style={styles.textStyle}>{item.name}</Text>
             }}
         />
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
 
 export default ListScreen;
