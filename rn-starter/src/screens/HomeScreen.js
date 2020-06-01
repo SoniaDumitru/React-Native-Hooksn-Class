c4dd3e186ad1c4fb10fd88f6571ebac3c579b217
Sonia Dumitru <soni.dumitru@gmail.com>
2020-06-01 11:07:52 -0400

    coordinating state

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -18,6 +18,12 @@ const HomeScreen = ({ navigation }) => {
       <Button
         title="Go to Counter Demo"
         onPress={() => navigation.navigate('Counter')}/>
+      <Button
+        title="Go to Color Demo"
+        onPress={() => navigation.navigate('Color')}/>
+         <Button
+        title="Go to Square Demo"
+        onPress={() => navigation.navigate('Square')}/>
     </View>
   );
 };
