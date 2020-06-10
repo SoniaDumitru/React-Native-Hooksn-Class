b05f31a28e5b0da89b2353cac34012adce1135a8
SoniaDumitru <sonya_dumitru@yahoo.com>
2020-06-10 10:03:58 -0400

    fixed scroll vertical and horizontal

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import ResultsDetail from './ResultsDetail';
 
-const ResultsList = () => {
+const ResultsList = ({ title, results }) => {
     return (
-        <View>
-            <Text>  
-               Results List: 
+        <View style={styles.container}>
+            <Text style={styles.titleStyle}>  
+               {title}
             </Text>
+            <FlatList
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                data={results}
+                keyExtractor={(results) => results.id}
+                renderItem={({item}) => {
+                    return <ResultsDetail result={item}/>
+                }}
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    titleStyle : {
+        fontSize: 18, 
+        fontWeight: 'bold',
+        margin: 15,
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    container: {
+        marginBottom: 10
+    }
+});
 
 export default ResultsList;
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
@@ -8,8 +8,14 @@ const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price
+        });
+    };
+
     return(
-        <View>
+        <View style={ {flex:1} }>
             <SearchBar 
                 term={term} 
                 onTermChange={setTerm}
@@ -17,9 +23,17 @@ const SearchScreen = () => {
             </SearchBar>
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found: {results.length} results</Text>
-            <ResultsList />
-            <ResultsList />
-            <ResultsList />
+            <ScrollView>
+                <ResultsList 
+                    results={filterResultsByPrice('$')}
+                    title='Cost Effective'/>
+                <ResultsList 
+                    results={filterResultsByPrice('$$')}
+                    title='Bit Pricier'/>
+                <ResultsList 
+                    results={filterResultsByPrice('$$$')}
+                    title='Big Spender'/>
+            </ScrollView>
         </View>
     );
 };
