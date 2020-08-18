3b72e52854bd39bf7b36fa2b28827448947dce2d
SoniaDumitru <sonya_dumitru@yahoo.com>
2020-08-18 15:59:55 -0500

    example using useContext()

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState }from 'react';
 
 // Object that's going responsable for moving information from Blog Post Provider to BlogList (nested child)
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+    const [blogPosts, setBlogPosts] = useState([]);
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title: `BlogPost #${blogPosts.length + 1}`}]);
+    };
+
     return (
-        <BlogContext.Provider value={'hi there'}>
+        <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
             {children}
         </BlogContext.Provider>
     );
--- blog/src/screens/indexScreen.js
+++ blog/src/screens/indexScreen.js
@@ -1,14 +1,25 @@
 import React, { useContext } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import BlogContext, { BlogProvider } from '../context/BlogContext';
 
 const IndexScreen = () => {
-const value = useContext(BlogContext);
-
+const blogPosts = useContext(BlogContext);
+const { data, addBlogPost } = useContext(BlogContext);
   return (
     <View style={styles.container}>
       <Text>Index Screen</Text>
-      <Text>{value}</Text>
+      <Button 
+        title="Add Post"
+        onPress={addBlogPost}
+      />
+      <FlatList 
+        data={data}
+        keyExtractor={(blogPosts)=>blogPosts.title}
+        renderItem={({item}) => {
+          return <Text>{item.title}</Text>
+        }}
+      >
+      </FlatList>
     </View>
   );
 };
