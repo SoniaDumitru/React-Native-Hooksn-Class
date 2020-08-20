4eab9860b34276b9a71ab84f64bf868481815b1d
SoniaDumitru <sonya_dumitru@yahoo.com>
2020-08-20 10:31:23 -0500

    added styling for rows

--- blog/src/screens/indexScreen.js
+++ blog/src/screens/indexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
@@ -13,13 +14,33 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return(
+          <View style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+            <EvilIcons style={styles.icon} name="trash" size={24} color="black" />
+          </View>
+          )
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    borderTopWidth: 1,
+    borderColor: 'gray',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingVertical: 10,
+  },
+  title : {
+    fontSize: 18
+  },
+  icon : {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
