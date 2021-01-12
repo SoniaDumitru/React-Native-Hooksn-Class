12cdf92d3dc6a213d5c238b8b85055f2a66b39b5
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-12 12:19:02 -0600

    added functionality to delete a blog post

--- blog_2/src/screens/IndexScreen.js
+++ blog_2/src/screens/IndexScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
@@ -7,22 +7,25 @@ import { Feather } from '@expo/vector-icons';
 import styles from './IndexScreen.styles';
 
 const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context);
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.row}>
+        <Text style={styles.title}>{item.title} - {item.id}</Text>
+        <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+          <Feather style={styles.icon} name="trash" />
+        </TouchableOpacity>
+      </View>
+    );
+  }
   return (
     <View>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
-        renderItem={({ item }) => {
-          return (
-            <View style={styles.row}>
-              <Text style={styles.title}>{item.title}</Text>
-              <Feather style={styles.icon} name="trash" />
-            </View>
-          );
-        }}
+        renderItem={(item) => renderItem(item)}
       />
     </View>
   );
