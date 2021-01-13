7dae5db0e73b21e5ddb750dfae4c68ba4ea13585
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-13 17:02:40 -0600

    added SALT and hash for the password

--- track-server/src/models/User.js
+++ track-server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // create user model
 const userSchema = new mongoose.Schema({
@@ -12,5 +13,42 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+userSchema.pre('save', function() {
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    // generate SALT and hash it
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// test password if they match
+userSchema.methods.comparePassword = function(candidatePassword) {
+    const user = this;
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+            if (err) {
+                return reject(err);
+            } 
+            if (!isMatch) {
+                return reject(false);
+            }
+            resolve(true);
+        });
+    })
+}
 
 mongoose.model('User', userSchema);
