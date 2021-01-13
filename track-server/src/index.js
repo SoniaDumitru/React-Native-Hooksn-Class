fb3c57a30a515191224c38c85666139e61cdf66a
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-13 15:12:10 -0600

    initiated post request

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/authRoutes');
 
 const app = express();
 
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://soniadumitru:passwordpassword@cluster0.a2inz.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+    userNewUrlParser: true,
+    userCreateIndex: true, 
+});
+
+// connect to mongose instance
+mongoose.connection.on('connect', () => {
+    console.log('Connect to mongo instance');
+});
+
+// announce when getting an error
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to mongo instance', err);
+});
+
 // route handler
-// req = http request
-// res = response object
 app.get('/', (req, res) => {
     res.send('hi there!')
 });
