95411110c084242bc120f5a3f9f1e2bbb6e7fb51
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-14 10:41:59 -0600

    added tracks client side

--- track-server/src/routes/trackRoutes.js
+++ track-server/src/routes/trackRoutes.js
@@ -12,8 +12,28 @@ router.use(requireAuth);
 // user can acces all tracks based on userId
 router.get('/tracks', async (req, res) => {
     const tracks = await Track.find({ userId: req.user._id });
+  
     res.send(tracks);
-});   
+  });
+
+// when a track is posted needs to have a name & array locations
+router.post('/tracks', async (req, res) => {
+    const { name, locations } = req.body;
+  
+    if (!name || !locations) {
+      return res
+        .status(422)
+        .send({ error: 'You must provide a name and locations' });
+    }
+  
+    try {
+      const track = new Track({ name, locations, userId: req.user._id });
+      await track.save();
+      res.send(track);
+    } catch (err) {
+      res.status(422).send({ error: err.message });
+    }
+  });
 
 module.exports = router;
 
