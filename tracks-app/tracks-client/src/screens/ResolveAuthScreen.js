a57a956e21a74805b0cda7c3dc29d469758cad95
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-21 13:08:05 -0600

    fixed flash of the signup screen, because when the app starts upp we show nothhing with that empty component

---
+++ tracks-app/tracks-client/src/screens/ResolveAuthScreen.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useContext } from 'react';
+import AuthForm from '../components/AuthForm';
+import { Context as AuthContext } from '../context/AuthContext';
+
+/**
+ * ResolveAuthScreen.
+ *
+ * ResolveAuthScreen resolve current user authentification status.
+ *
+ * @param {object} props - Screen Props.
+ *
+ * @returns {React.ReactElement} - React element.
+ */
+
+const ResolveAuthScreen = () => {
+    const { tryLocalSignin } = useContext(AuthContext);
+
+    useEffect(() => {
+        tryLocalSignin();
+    }), [];
+    return null;
+};
