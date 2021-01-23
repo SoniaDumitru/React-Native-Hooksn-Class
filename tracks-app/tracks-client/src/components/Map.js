6ad684796b7e829002d28461ba8478f9a2acc85e
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-23 15:30:59 -0600

    completed scaffolding for map component

---
+++ tracks-app/tracks-client/src/components/Map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const Map = () => {
+    return <Text>I am map</Text>
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Map;
--- tracks-app/tracks-client/src/screens/TrackCreateScreen.js
+++ tracks-app/tracks-client/src/screens/TrackCreateScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>;
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Map/>
+      <Text h2 style={{ fontSize: 48 }}>Create a Track</Text>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
