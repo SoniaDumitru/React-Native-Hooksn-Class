43c51b6378159140de0a4b0f0599b8760788559c
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-23 15:51:51 -0600

    added initial region on map

--- tracks-app/tracks-client/src/screens/TrackCreateScreen.js
+++ tracks-app/tracks-client/src/screens/TrackCreateScreen.js
@@ -3,12 +3,13 @@ import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import Map from '../components/Map';
+import MapView from 'react-native-maps';
 
 const TrackCreateScreen = () => {
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
-      <Map/>
       <Text h2 style={{ fontSize: 48 }}>Create a Track</Text>
+      <Map/>
     </SafeAreaView>
   );
 };
